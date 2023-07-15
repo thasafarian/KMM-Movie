8053e5658d1b89a950194c15ec30674ee3261cf4
thasafarian <farian.tasa@gmail.com>
2023-07-15 10:10:35 +0700

    initial project setup

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "My_Movie_App"
+include(":androidApp")
+include(":shared")
